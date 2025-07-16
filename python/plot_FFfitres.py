77c052f10373cf105aa554c893d508ae079cb0be
David Vico Benet <david.vico.benet@cern.ch>
2025-07-16 18:13:52 +0200

    FF fit changes and plotting

---
+++ python/plot_FFfitres.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+def readres(inpath: str, label: str = None):
+    with open(inpath) as f:
+        d = json.load(f)
+    if label:
+        d["label"] = label
+    else:
+        d["label"] = inpath
+    return d
+
+def setPlotParams(params = {}):
+    """Set of parameters for plots in matplotlib
+    """
+    if len(params) > 0:
+        plt.rcParams.update(params)
+    else:
+        font = {'family' : 'cmr10', #Or Times New Roman
+            'size'   : 14}
+        plt.rc('font', **font)
+        plt.rcParams['mathtext.fontset'] = 'cm'
+        plt.rcParams["axes.formatter.use_mathtext"] = True
+        plt.rcParams.update({'axes.labelsize': 14,
+                            'legend.fontsize': 10,
+                            'xtick.labelsize': 12,
+                            'ytick.labelsize': 12,
+                            'figure.figsize': [8, 8/1.618]})
+
+
+
+def makePlotHorizontal(obsdicts_list, labels, obslist, errorsonly, savepath):
+    x_base = np.arange(1, len(obslist)+1, 1)
+    plt.clf()
+    plt.figure(figsize = (12, 4.5))
+    maxabs = 0
+    for i in range(len(obsdicts_list)):
+        obs = obsdicts_list[i]["val"] if not errorsonly else np.zeros_like(obsdicts_list[i]["val"])
+        err = obsdicts_list[i]["err"]
+        x = x_base + i*0.5/(len(obsdicts_list)-1)
+        if max(np.abs(obs)+np.abs(err)) > maxabs:
+            maxabs = max(np.abs(obs)+np.abs(err))
+        # print(obs)
+        # plt.plot(x, obs, 'x', label=labels[i])
+        plt.errorbar(x, obs, yerr=err, markersize=2, capsize=2, label=labels[i], fmt='o',)
+
+
+    # plt.axhline(0.0, 0, 15, color='grey', alpha = 0.5, zorder=-5)
+    tick_p = x_base + 0.25
+    plt.gca().set_xticks(tick_p, obslist)
+    
+    ylim = [-maxabs-0.05, maxabs+0.05]
+    plt.xlim(0.5, x_base[-1]+1)
+    plt.ylim(ylim[0], ylim[1])
+
+    for elem in tick_p:
+        # plt.axhline(elem, color='grey', alpha = 0.5, linestyle='dashed')
+        # plt.fill_between(ylim, elem-0.45, elem+0.45, zorder=-5, color='grey', alpha=0.1)
+        plt.gca().axvspan(elem-0.45, elem+0.45, zorder=-5, color='grey', alpha=0.1)
+    
+    plt.legend(bbox_to_anchor=(1.01, 1.0))
+    plt.tight_layout()
+    plt.savefig(savepath, bbox_inches = 'tight', dpi=150)
+
+
+
+
+
+setPlotParams()
+results2221 = [
+    "output/fitres_FFBGL2221_N2e6_B3_3_5.json",
+    "output/fitres_FFBGL2221_N2e6_B4_4_5.json"
+]
+labels = [
+    r"$N=2\times 10^6$, (3, 3, 5)",
+    r"$N=2\times 10^6$, (4, 4, 5)",
+]
+res = [readres(i) for i in results2221]
+makePlotHorizontal(res, labels, [r"$a_0$", r"$a_1$", r"$a_2$", r"$b_1$", r"$b_2$", r"$c_1$", r"$c_2$", r"$d_0$", r"$d_1$"], errorsonly=False, savepath="output/BGLfitres2221_compare.png")
+makePlotHorizontal(res, labels, [r"$a_0$", r"$a_1$", r"$a_2$", r"$b_1$", r"$b_2$", r"$c_1$", r"$c_2$", r"$d_0$", r"$d_1$"], errorsonly=True, savepath="output/BGLfitres2221_compare_err.png")
+
+
+results2221 = [
+    "output/fitres_FFBGL1110_N2e6_B3_3_5.json",
+    "output/fitres_FFBGL1110_N2e6_B4_4_5.json"
+]
+res = [readres(i) for i in results2221]
+makePlotHorizontal(res, labels, [r"$a_0$", r"$a_1$", r"$b_1$", r"$c_1$", r"$d_0$"], errorsonly=False, savepath="output/BGLfitres1110_compare.png")
+makePlotHorizontal(res, labels, [r"$a_0$", r"$a_1$", r"$b_1$", r"$c_1$", r"$d_0$"], errorsonly=True, savepath="output/BGLfitres1110_compare_err.png")
--- python/test_FF_fit.py
+++ python/test_FF_fit.py
@@ -17,21 +17,39 @@ from bd2dstlnu.Predictions.BToDstObs import BToDstEllNuPrediction
 from bd2dstlnu.Predictions.BToDstFFBGL import BGL
 
 obs = BToDstEllNuPrediction("mu", "mu", BGL)
-{ # https://arxiv.org/pdf/1707.09509 (Third column in table V)
+# { # https://arxiv.org/pdf/1707.09509 (Third column in table V)
+#     "a0" : 0.0209,
+#     "a1" : 0.33,
+#     "a2" : 0.6,
+#     "b0" : 0.01218,
+#     "b1" : 0.046,
+#     "b2" : 0.48,
+#     "c1" : 0.0063,
+#     "c2" : 0.062,
+#     "d0" : 0.0595,
+#     "d1" : -0.218
+# }
+ff_gen = { # https://arxiv.org/pdf/1707.09509 (Third column in table V)
     "a0" : 0.0209,
     "a1" : 0.33,
-    "a2" : 0.6,
+    "a2" : 0.0,
     "b0" : 0.01218,
     "b1" : 0.046,
-    "b2" : 0.48,
+    "b2" : 0.0,
     "c1" : 0.0063,
-    "c2" : 0.062,
+    "c2" : 0.0,
     "d0" : 0.0595,
-    "d1" : -0.218
+    "d1" : 0.0
 }
-# Gets a histogram of the PDF with specified binning
-h, b, angint, j_bins = obs.PDF_hist(10, 3, 3, 5)
+obs.set_ff(ff_gen)
+print("Generated Values:")
+print(obs.FF.ffpar)
 
+ctd_bins = 4
+ctl_bins = 4
+chi_bins = 5
+# Gets a histogram of the PDF with specified binning
+h, b, angint, j_bins = obs.PDF_hist(10, ctd_bins, ctl_bins, chi_bins)
 
 N = 2e6
 h_norm = h/np.sum(h)
@@ -44,17 +62,29 @@ pred = BToDstEllNuPrediction("mu", "mu", BGL)
 
 
 def calc_pdf(ff: list[float]):
+    # ffd = {
+    #     "a0" : ff[0],
+    #     "a1" : ff[1],
+    #     "a2" : ff[2],
+    #     "b0" : 0.01218,
+    #     "b1" : ff[3],
+    #     "b2" : ff[4],
+    #     "c1" : ff[5],
+    #     "c2" : ff[6],
+    #     "d0" : ff[7],
+    #     "d1" : ff[8],
+    # }
     ffd = {
         "a0" : ff[0],
         "a1" : ff[1],
-        "a2" : ff[2],
+        "a2" : 0.0,
         "b0" : 0.01218,
-        "b1" : ff[3],
-        "b2" : ff[4],
-        "c1" : ff[5],
-        "c2" : ff[6],
-        "d0" : ff[7],
-        "d1" : ff[8],
+        "b1" : ff[2],
+        "b2" : 0.0,
+        "c1" : ff[3],
+        "c2" : 0.0,
+        "d0" : ff[4],
+        "d1" : 0.0,
     }
     pred.set_ff(ffd)
     print(pred.FF.ffpar)
@@ -72,7 +102,8 @@ def fom(ff: list[float]):
     return chiSq
 
 
-fitpars = ["a0", "a1", "a2", "b1", "b2", "c1", "c2", "d0", "d1"]
+# fitpars = ["a0", "a1", "a2", "b1", "b2", "c1", "c2", "d0", "d1"]
+fitpars = ["a0", "a1", "b1", "c1", "d0"]
 init_vals = [obs.FF.ffpar[ipar] for ipar in fitpars]
 m = Minuit(fom, init_vals)
 m.migrad()
@@ -100,8 +131,9 @@ import json
 res = {
     "val" : reslist,
     "err" : errlist,
-    "pars" : fitpars
+    "pars" : fitpars,
+    "gen" : ff_gen,
 }
-outpath = "output/FF_bgl_results.json"
+outpath = f"output/fitres_FFBGL1110_N2e6_B{ctd_bins}_{ctl_bins}_{chi_bins}.json"
 with open(outpath, "w") as outfile:
     json.dump(res, outfile, indent=2)
