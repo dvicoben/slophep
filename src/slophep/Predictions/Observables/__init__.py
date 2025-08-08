d5ad64aeb62d130972033a3fb1530a45ebf36a30
David Vico Benet <david.vico.benet@cern.ch>
2025-08-08 13:27:46 +0200

    fixed init

--- src/slophep/Predictions/Observables/__init__.py
+++ src/slophep/Predictions/Observables/__init__.py
@@ -3,10 +3,12 @@ from slophep.Predictions.Observables.BToVEllNuObs import BToVEllNuPrediction
 from slophep.Predictions.Observables.BToPEllNuObs import BToPEllNuPrediction
 from slophep.Predictions.Observables.ObservablesBToV import BToDstEllNuPrediction
 from slophep.Predictions.Observables.ObservablesBToV import BsToDsstEllNuPrediction
+from slophep.Predictions.Observables.ObservablesBToP import BToDEllNuPrediction
 
 
 __all__ = [
     "ObservableBase",
     "BToVEllNuPrediction", "BToPEllNuPrediction", 
-    "BToDstEllNuPrediction", "BsToDsstEllNuPrediction"
+    "BToDstEllNuPrediction", "BsToDsstEllNuPrediction",
+    "BToDEllNuPrediction"
 ]
