b18563e70d0c859ec41decf07a4f48b0689948c4
David Vico Benet <david.vico.benet@cern.ch>
2025-08-08 11:51:13 +0200

    updated predictions to inherit from base class

--- src/slophep/Predictions/Observables/BToPEllNuObs.py
+++ src/slophep/Predictions/Observables/BToPEllNuObs.py
@@ -1,6 +1,7 @@
 from math import sqrt
 
 from slophep.Predictions.FormFactorsBToP import FormFactorBToP
+from slophep.Predictions.Observables import ObservableBase
 
 import flavio
 from flavio.physics.running import running
@@ -8,7 +9,7 @@ from flavio.physics.bdecays.wilsoncoefficients import get_wceff_fccc
 from flavio.physics.bdecays import angular
 from flavio.physics import ckm
 
-class BToPEllNuPrediction:
+class BToPEllNuPrediction(ObservableBase):
     def __init__(self, 
                  B: str,
                  P: str,
@@ -20,29 +21,14 @@ class BToPEllNuPrediction:
                  par: dict = None,
                  scale: float = 4.8,
                  ):
+        super().__init__(FF, ffargs, par, scale)
         self._B: str = B
         self._P: str = P
         self._qiqj: str = qiqj
         self._lep: str = lep
         self._nu: str = nu
-        self._wc_obj: flavio.WilsonCoefficients = flavio.WilsonCoefficients()
-
-        self._par: dict = flavio.default_parameters.get_central_all()
-        if type(par) == dict:
-            self._par = par
-        self._scale: float = scale
-
-        self._FF: FormFactorBToP = FF(par, scale, *ffargs)
         self.obslist = ['a', 'b', 'c']
 
-    @property
-    def scale(self) -> float: 
-        """Renorm scale"""
-        return self._scale
-    @property
-    def par(self) -> dict: 
-        """Dictionary of parameters, defaults to `flavio.default_parameters.get_central_all()`"""
-        return self._par
     @property
     def B(self) -> str:
         """The B meson"""
@@ -72,91 +58,7 @@ class BToPEllNuPrediction:
         mP = self.par['m_'+self._P]
         q2max = (mB-mP)**2
         return q2max
-    @property
-    def wc_obj(self) -> flavio.WilsonCoefficients: 
-        """Wilson coefficient object"""
-        return self._wc_obj
-    @property
-    def FF(self) -> FormFactorBToP: 
-        """Form factors"""
-        return self._FF
-
-    def set_wc(self, wc_dict: dict, eft: str = 'WET', basis: str = 'flavio'):
-        """Set the wilson coefficients
-
-        Parameters
-        ----------
-        wc_dict : dict
-            Dictionary of wilson coefficients
-        eft : str, optional
-            EFT, by default 'WET'
-        basis : str, optional
-            WC basis (see https://wcxf.github.io/bases.html), by default 'flavio'
-        """
-        self._wc_obj.set_initial(wc_dict, self.scale, eft, basis)
-
-    def set_ff(self, ffparams: dict):
-        """Set form factor parameters. Can use None to leave a parameter unchanged.
-
-        Parameters
-        ----------
-        ffparams : dict
-            Dictionary of form factor parameters, names should match FF.ffpars dictionary in the particular 
-            scheme being used
-        """
-        self._FF.set_ff(**ffparams)
     
-    def _fullsetter(self, params: dict, constants: dict = {}):
-        """Set WCs and FF parameters, for usage with Fluctuate
-
-        FF parameters must follow naming in self.FF.params
-
-        WCs must be in the flavio basis, prepended with 'WCRe\_' or 'WCIm\_'
-        for the respective component
-
-        Parameters
-        ----------
-        params : dict
-            Dictionary of parameters to set
-        constants: dict
-            Dictinoary of parameters that are set constant - for specific use-cases with fluctuate
-            e.g. in case want to set a particular WC to some non-zero value for all fluctations. In
-            principle FF that are not in params should be kept the same so shouldn't need to
-            pass them here.
-        """
-        # Note no key checking performed! User must make sure these are set-up correctly!
-        par = {**params, **constants}
-        wc = {}
-        ff = {}
-        for ikey, ival in par.items():
-            # Handle Wilson Coefficients
-            if "WCRe_" in ikey or "WCIm_" in ikey:
-                name_wc = ikey[5:]
-                iwc = ival if "WCRe_" in ikey else 1.0j*ival
-                if name_wc not in wc:
-                    wc[name_wc] = 0.0
-                wc[name_wc] += iwc
-            # Everything else is considered a FF
-            else:
-                ff[ikey] = ival
-        
-        if len(wc) > 0:
-            self.set_wc(wc)
-        if len(ff) > 0:
-            self.set_ff(ff)
-
-
-    def set_ff_fromlist(self, ffparams: list):
-        """Set form factor parameters in form of list. Must include all parameters in self.FF.params, in order.
-        Can use None to leave a parameter unchanged.
-
-        Parameters
-        ----------
-        ffparams : list
-            All FF parameters, in appropiate order
-        """
-        self._FF.set_ff_fromlist(ffparams)
-
     def _prefactor(self, q2: float) -> float:
         """Return the prefactor including constants and CKM elements. Direct reimplementation
         of flavio equivalent in https://flav-io.github.io/apidoc/flavio/physics/bdecays/bplnu.m.html
--- src/slophep/Predictions/Observables/BToVEllNuObs.py
+++ src/slophep/Predictions/Observables/BToVEllNuObs.py
@@ -13,7 +13,7 @@ from flavio.physics.bdecays import angular
 from flavio.physics import ckm
 
 
-class BToVEllNuPrediction:
+class BToVEllNuPrediction(ObservableBase):
     def __init__(self, 
                  B: str,
                  V: str,
@@ -25,29 +25,14 @@ class BToVEllNuPrediction:
                  par: dict = None,
                  scale: float = 4.8,
                  ):
+        super().__init__(FF, ffargs, par, scale)
         self._B: str = B
         self._V: str = V
         self._qiqj: str = qiqj
         self._lep: str = lep
         self._nu: str = nu
-        self._wc_obj: flavio.WilsonCoefficients = flavio.WilsonCoefficients()
-
-        self._par: dict = flavio.default_parameters.get_central_all()
-        if type(par) == dict:
-            self._par = par
-        self._scale: float = scale
-
-        self._FF: FormFactorBToV = FF(par, scale, *ffargs)
         self.obslist = ['1s', '1c', '2s', '2c', '6s', '6c', 3, 4, 5, 7, 8, 9]
 
-    @property
-    def scale(self) -> float: 
-        """Renorm scale"""
-        return self._scale
-    @property
-    def par(self) -> dict: 
-        """Dictionary of parameters, defaults to `flavio.default_parameters.get_central_all()`"""
-        return self._par
     @property
     def B(self) -> str:
         """The B meson"""
@@ -77,90 +62,6 @@ class BToVEllNuPrediction:
         mV = self.par['m_'+self._V]
         q2max = (mB-mV)**2
         return q2max
-    @property
-    def wc_obj(self) -> flavio.WilsonCoefficients: 
-        """Wilson coefficient object"""
-        return self._wc_obj
-    @property
-    def FF(self) -> FormFactorBToV: 
-        """Form factors"""
-        return self._FF
-
-    def set_wc(self, wc_dict: dict, eft: str = 'WET', basis: str = 'flavio'):
-        """Set the wilson coefficients
-
-        Parameters
-        ----------
-        wc_dict : dict
-            Dictionary of wilson coefficients
-        eft : str, optional
-            EFT, by default 'WET'
-        basis : str, optional
-            WC basis (see https://wcxf.github.io/bases.html), by default 'flavio'
-        """
-        self._wc_obj.set_initial(wc_dict, self.scale, eft, basis)
-
-    def set_ff(self, ffparams: dict):
-        """Set form factor parameters. Can use None to leave a parameter unchanged.
-
-        Parameters
-        ----------
-        ffparams : dict
-            Dictionary of form factor parameters, names should match FF.ffpars dictionary in the particular 
-            scheme being used
-        """
-        self._FF.set_ff(**ffparams)
-    
-    def _fullsetter(self, params: dict, constants: dict = {}):
-        """Set WCs and FF parameters, for usage with Fluctuate
-
-        FF parameters must follow naming in self.FF.params
-
-        WCs must be in the flavio basis, prepended with 'WCRe\_' or 'WCIm\_'
-        for the respective component
-
-        Parameters
-        ----------
-        params : dict
-            Dictionary of parameters to set
-        constants: dict
-            Dictinoary of parameters that are set constant - for specific use-cases with fluctuate
-            e.g. in case want to set a particular WC to some non-zero value for all fluctations. In
-            principle FF that are not in params should be kept the same so shouldn't need to
-            pass them here.
-        """
-        # Note no key checking performed! User must make sure these are set-up correctly!
-        par = {**params, **constants}
-        wc = {}
-        ff = {}
-        for ikey, ival in par.items():
-            # Handle Wilson Coefficients
-            if "WCRe_" in ikey or "WCIm_" in ikey:
-                name_wc = ikey[5:]
-                iwc = ival if "WCRe_" in ikey else 1.0j*ival
-                if name_wc not in wc:
-                    wc[name_wc] = 0.0
-                wc[name_wc] += iwc
-            # Everything else is considered a FF
-            else:
-                ff[ikey] = ival
-        
-        if len(wc) > 0:
-            self.set_wc(wc)
-        if len(ff) > 0:
-            self.set_ff(ff)
-
-
-    def set_ff_fromlist(self, ffparams: list):
-        """Set form factor parameters in form of list. Must include all parameters in self.FF.params, in order.
-        Can use None to leave a parameter unchanged.
-
-        Parameters
-        ----------
-        ffparams : list
-            All FF parameters, in appropiate order
-        """
-        self._FF.set_ff_fromlist(ffparams)
 
     def _prefactor(self, q2: float) -> float:
         """Return the prefactor including constants and CKM elements. Direct reimplementation
