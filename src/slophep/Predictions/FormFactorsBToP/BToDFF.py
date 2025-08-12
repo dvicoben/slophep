09daeada7586d03088cb6a08eb54e7f73bd8e0f3
David Vico Benet <david.vico.benet@cern.ch>
2025-08-12 13:27:28 +0200

    added B->P CLN

--- src/slophep/Predictions/FormFactorsBToP/BToDFF.py
+++ src/slophep/Predictions/FormFactorsBToP/BToDFF.py
@@ -1,6 +1,7 @@
 from slophep.Predictions.FormFactorsBToP import BSZ_BToP
 from slophep.Predictions.FormFactorsBToP import BLPR_BToP
 from slophep.Predictions.FormFactorsBToP import BGL_BToP
+from slophep.Predictions.FormFactorsBToP import CLN_BToP
 
 class BSZ(BSZ_BToP):
     def __init__(self, par: dict = None, scale: float = None, *ffargs):
@@ -11,5 +12,9 @@ class BLPR(BLPR_BToP):
         super().__init__("B+", "D0", par, scale, *ffargs)
 
 class BGL(BGL_BToP):
+    def __init__(self, par: dict = None, scale: float = None, *ffargs):
+        super().__init__("B+", "D0", par, scale, *ffargs)
+
+class CLN(CLN_BToP):
     def __init__(self, par: dict = None, scale: float = None, *ffargs):
         super().__init__("B+", "D0", par, scale, *ffargs)
--- src/slophep/Predictions/FormFactorsBToP/__init__.py
+++ src/slophep/Predictions/FormFactorsBToP/__init__.py
@@ -2,10 +2,11 @@ from slophep.Predictions.FormFactorsBToP.BToPFFBase import FormFactorBToP
 from slophep.Predictions.FormFactorsBToP.BToPFFBSZ import BSZ_BToP
 from slophep.Predictions.FormFactorsBToP.BToPFFBLPR import BLPR_BToP
 from slophep.Predictions.FormFactorsBToP.BToPFFBGL import BGL_BToP
+from slophep.Predictions.FormFactorsBToP.BToPFFCLN import CLN_BToP
 import slophep.Predictions.FormFactorsBToP.BToDFF as BToDFF
 
 __all__ = [
     "FormFactorBToP",
-    "BSZ_BToP", "BLPR_BToP", "BGL_BToP",
+    "BSZ_BToP", "BLPR_BToP", "BGL_BToP", "CLN_BToP",
     "BToDFF"
 ]
