8cb18bb32fa5cab76f11cbbcda6be0540c864c80
David Vico Benet <david.vico.benet@cern.ch>
2025-08-13 16:33:36 +0200

    cleaned up eos checks

--- checks/checks_eos.py
+++ checks/checks_eos.py
@@ -4,7 +4,7 @@ from slophep.Predictions.FormFactorsBToV import BToDstFF
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Getting the SLOP predictions for BSZ:
 def get_spectrum(ff):
     q2max = (ff.internalparams["Mb"] - ff.internalparams["Mc"])**2
     qsq = np.linspace(0.012, q2max-1e-3, 100)
@@ -26,10 +26,8 @@ btodst_bsz = BToDstFF.BSZ()
 btodst_qsq, btodst_bszff = get_spectrum(btodst_bsz)
 
 
-# Getting the EOS predictions
+# Getting the EOS predictions:
 import eos
-
-parameters = eos.Parameters()
 slop_to_eos = {
     "f+"  : "{}::f_+(q2)",
     "f0"  : "{}::f_0(q2)",
@@ -42,24 +40,25 @@ slop_to_eos = {
     "T2"  : "{}::T_2(q2)",
     "T23" : "{}::T_23(q2)",
 }
-options = eos.Options({
-    'l' : "mu",
-    'q' : 'd',
-    'model' : 'WET',
-    'form-factors' : 'BSZ2015'
-})
 
-def get_spectrum_eos(ffs: list, qsq, mode):
+def get_spectrum_eos(ffs: list, qsq, mode, ffscheme):
+    options = eos.Options({
+        'l' : "mu",
+        'q' : 'd',
+        'model' : 'WET',
+        'form-factors' : ffscheme
+    })
     ff = {}
     for iff in ffs:
         eosff = slop_to_eos[iff].format(mode)
-        ffeval = [eos.Observable.make(eosff, parameters, eos.Kinematics(q2=iq2), options).evaluate() for iq2 in qsq]
+        ffeval = [eos.Observable.make(eosff, eos.Parameters(), eos.Kinematics(q2=iq2), options).evaluate() for iq2 in qsq]
         ff[iff] = np.array(ffeval)
     return ff
 
 
-btod_bszeos = get_spectrum_eos(["f+", "f0", "fT"], btod_qsq, "B->D")
-btodst_bszeos = get_spectrum_eos(["A0", "A1", "A12", "V", "T1", "T2", "T23"], btodst_qsq, "B->D^*")
+btod_bszeos = get_spectrum_eos(["f+", "f0", "fT"], btod_qsq, "B->D", "BSZ2015")
+btodst_bszeos = get_spectrum_eos(["A0", "A1", "A12", "V", "T1", "T2", "T23"], btodst_qsq, "B->D^*", "BSZ2015")
+# bstodsst_bszeos = get_spectrum_eos(["A0", "A1", "A12", "V", "T1", "T2", "T23"], btodst_qsq, "B_s->D_s^*", "BGJvD2019")
 
 
 # Plotting them together
