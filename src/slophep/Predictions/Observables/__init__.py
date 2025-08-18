3a58d96ccc6db12402c3dae0278db95577068b74
David Vico Benet <david.vico.benet@cern.ch>
2025-08-18 11:25:08 +0200

    init fix, docs update

--- src/slophep/Predictions/Observables/__init__.py
+++ src/slophep/Predictions/Observables/__init__.py
@@ -1,14 +1,13 @@
 from slophep.Predictions.Observables.ObservableBase import ObservableBase
 from slophep.Predictions.Observables.BToVEllNuObs import BToVEllNuPrediction
 from slophep.Predictions.Observables.BToPEllNuObs import BToPEllNuPrediction
-from slophep.Predictions.Observables.ObservablesBToV import BdToDstEllNuPrediction
-from slophep.Predictions.Observables.ObservablesBToV import BsToDsstEllNuPrediction
-from slophep.Predictions.Observables.ObservablesBToP import BpToDEllNuPrediction
+from slophep.Predictions.Observables.ObservablesBToV import BdToDstEllNuPrediction, BsToDsstEllNuPrediction
+from slophep.Predictions.Observables.ObservablesBToP import BpToDEllNuPrediction, BdToDEllNuPrediction
 
 
 __all__ = [
     "ObservableBase",
     "BToVEllNuPrediction", "BToPEllNuPrediction", 
     "BdToDstEllNuPrediction", "BsToDsstEllNuPrediction",
-    "BpToDEllNuPrediction"
+    "BpToDEllNuPrediction", "BdToDEllNuPrediction"
 ]
