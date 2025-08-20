bbaf7f7e34f0445753f04205b6c456edbae8049c
David Vico Benet <david.vico.benet@cern.ch>
2025-08-20 10:33:23 +0200

    nomenclature fixes

--- src/slophep/Predictions/FormFactorsBToP/__init__.py
+++ src/slophep/Predictions/FormFactorsBToP/__init__.py
@@ -3,10 +3,10 @@ from slophep.Predictions.FormFactorsBToP.BToPFFBSZ import BSZ_BToP
 from slophep.Predictions.FormFactorsBToP.BToPFFBLPR import BLPR_BToP
 from slophep.Predictions.FormFactorsBToP.BToPFFBGL import BGL_BToP
 from slophep.Predictions.FormFactorsBToP.BToPFFCLN import CLN_BToP
-import slophep.Predictions.FormFactorsBToP.BpToDFF as BpToDFF
+import slophep.Predictions.FormFactorsBToP.BuToDFF as BuToDFF
 
 __all__ = [
     "FormFactorBToP",
     "BSZ_BToP", "BLPR_BToP", "BGL_BToP", "CLN_BToP",
-    "BpToDFF"
+    "BuToDFF"
 ]
