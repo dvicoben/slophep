76effe738f9a86505ecd4deec5a7467316c36826
David Vico Benet <david.vico.benet@cern.ch>
2026-01-07 18:58:52 +0100

    BdToDst BLPRXP

--- src/slophep/Predictions/FormFactorsBToV/__init__.py
+++ src/slophep/Predictions/FormFactorsBToV/__init__.py
@@ -4,6 +4,7 @@ from slophep.Predictions.FormFactorsBToV.BToVFFBGL import BGL_BToV
 from slophep.Predictions.FormFactorsBToV.BToVFFCLN import CLN_BToV
 from slophep.Predictions.FormFactorsBToV.BToVFFCLN2 import CLN2_BToV
 from slophep.Predictions.FormFactorsBToV.BToVFFBLPR import BLPR_BToV
+from slophep.Predictions.FormFactorsBToV.BToVFFBLPRXP import BLPRXP_BToV
 from slophep.Predictions.FormFactorsBToV.BToVFFBSZ import BSZ_BToV
 from slophep.Predictions.FormFactorsBToV.BToVFFHPQCD import HPQCD_BToV
 from slophep.Predictions.FormFactorsBToV.BToVFFHammerEquiv import BGL_BToV_Hammer, BLPR_BToV_Hammer, CLN_BToV_Hammer
@@ -12,6 +13,6 @@ import slophep.Predictions.FormFactorsBToV.BsToDsstFF as BsToDsstFF
 
 
 __all__ = ["FormFactorBToV", 
-           "ISGW2_BToV", "BGL_BToV", "CLN_BToV", "CLN2_BToV", "BLPR_BToV", "BSZ_BToV", "HPQCD_BToV",
+           "ISGW2_BToV", "BGL_BToV", "CLN_BToV", "CLN2_BToV", "BLPR_BToV", "BLPRXP_BToV", "BSZ_BToV", "HPQCD_BToV",
            "BGL_BToV_Hammer", "CLN_BToV_Hammer", "BLPR_BToV_Hammer",
            "BdToDstFF", "BsToDsstFF"]
