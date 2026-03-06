49d665077dccac550872a9d77d8a6ce4e818f9da
dvicoben <davidvicobenet2016@gmail.com>
2026-03-06 11:39:54 +0100

    added B->D2* BLR

--- src/slophep/Predictions/FormFactorsBToDstst/FFBuToD2st.py
+++ src/slophep/Predictions/FormFactorsBToDstst/FFBuToD2st.py
@@ -4,3 +4,9 @@ class ISGW2(FFBToDstst.ISGW2_BToD2st):
     def __init__(self, par: dict = None, scale: float = None, *ffargs):
         super().__init__("B+", "D2*0", par, scale, *ffargs)
         self._name = "BuToD2st_ISGW2"
+
+
+class BLR(FFBToDstst.BLR_BToD2st):
+    def __init__(self, par: dict = None, scale: float = None, *ffargs):
+        super().__init__("B+", "D2*0", par, scale, *ffargs)
+        self._name = "BuToD2st_BLR"
--- src/slophep/Predictions/FormFactorsBToDstst/__init__.py
+++ src/slophep/Predictions/FormFactorsBToDstst/__init__.py
@@ -6,6 +6,7 @@ from slophep.Predictions.FormFactorsBToDstst.BToD1stISGW2 import ISGW2_BToD1st
 from slophep.Predictions.FormFactorsBToDstst.BToD2stISGW2 import ISGW2_BToD2st
 from slophep.Predictions.FormFactorsBToDstst.BToD1FFBLR import BLR_BToD1
 from slophep.Predictions.FormFactorsBToDstst.BToD1stFFBLR import BLR_BToD1st
+from slophep.Predictions.FormFactorsBToDstst.BToD2stFFBLR import BLR_BToD2st
 import slophep.Predictions.FormFactorsBToDstst.FFBuToD1 as BuToD1FF
 import slophep.Predictions.FormFactorsBToDstst.FFBuToD1st as BuToD1stFF
 import slophep.Predictions.FormFactorsBToDstst.FFBuToD2st as BuToD2stFF
@@ -14,6 +15,6 @@ __all__ = [
     "FormFactorBToDz", "FormFactorBToD1", "FormFactorBToD1st", "FormFactorBToD2st",
     "ISGW2_BToD1", "BLR_BToD1",
     "ISGW2_BToD1st", "BLR_BToD1st",
-    "ISGW2_BToD2st",
+    "ISGW2_BToD2st", "BLR_BToD2st",
     "BuToD1FF", "BuToD1stFF", "BuToD2stFF"
 ]
