a514f0d43d0f3eda619b7c935340895896056845
dvicoben <davidvicobenet2016@gmail.com>
2026-03-06 11:25:29 +0100

    added D2st observable

--- src/slophep/Predictions/Observables/ObservablesBToDstst.py
+++ src/slophep/Predictions/Observables/ObservablesBToDstst.py
@@ -1,7 +1,9 @@
 from slophep.Predictions.FormFactorsBToDstst import FormFactorBToD1
 from slophep.Predictions.FormFactorsBToDstst import FormFactorBToD1st
+from slophep.Predictions.FormFactorsBToDstst import FormFactorBToD2st
 from slophep.Predictions.Observables.BToD1EllNuObs import BToD1EllNuPrediction
 from slophep.Predictions.Observables.BToD1stEllNuObs import BToD1stEllNuPrediction
+from slophep.Predictions.Observables.BToD2stEllNuObs import BToD2stEllNuPrediction
 
 class BuToD1EllNuPrediction(BToD1EllNuPrediction):
    def __init__(self, 
@@ -25,3 +27,14 @@ class BuToD1stEllNuPrediction(BToD1stEllNuPrediction):
                  scale: float = 4.8,
                  ):
         super().__init__("B+", "D1*0", "bc", lep, nu, FF, ffargs, par, scale)
+
+class BuToD2stEllNuPrediction(BToD2stEllNuPrediction):
+   def __init__(self, 
+                 lep: str, 
+                 nu: str,
+                 FF: FormFactorBToD2st,
+                 ffargs: list = [],
+                 par: dict = None,
+                 scale: float = 4.8,
+                 ):
+        super().__init__("B+", "D2*0", "bc", lep, nu, FF, ffargs, par, scale)
