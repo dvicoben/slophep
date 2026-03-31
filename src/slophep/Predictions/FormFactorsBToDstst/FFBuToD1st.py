445e5257a8dd2f72a3b0618e139a8d12d3660d7e
dvicoben <davidvicobenet2016@gmail.com>
2026-03-31 12:47:11 +0200

    added LLSW for D1st

--- src/slophep/Predictions/FormFactorsBToDstst/FFBuToD1st.py
+++ src/slophep/Predictions/FormFactorsBToDstst/FFBuToD1st.py
@@ -5,6 +5,10 @@ class ISGW2(FFBToDstst.ISGW2_BToD1st):
         super().__init__("B+", "D1*0", par, scale, *ffargs)
         self._name = "BuToD1st_ISGW2"
 
+class LLSW(FFBToDstst.LLSW_BToD1st):
+    def __init__(self, par: dict = None, scale: float = None, *ffargs):
+        super().__init__("B+", "D1*0", par, scale, *ffargs)
+        self._name = "BuToD1st_LLSW"
 
 class BLR(FFBToDstst.BLR_BToD1st):
     def __init__(self, par: dict = None, scale: float = None, *ffargs):
