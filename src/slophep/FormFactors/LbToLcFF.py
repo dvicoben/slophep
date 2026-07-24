9b534b5a9fe9d62a991c63a6015b3690541df97e
dvicoben <davidvicobenet2016@gmail.com>
2026-07-24 17:23:51 +0200

    baryonic Lb->Lc ff refactor

---
+++ src/slophep/FormFactors/LbToLcFF.py
@@ -0,0 +1,6 @@
+import slophep.FormFactors.FormFactorsBaryonic as FFBar
+
+class DKMR(FFBar.FFBaryonic_DKMR):
+    _name = "FFLbToLc@DKMR"
+    def __init__(self):
+        super().__init__("Lambdab", "Lambdac")
