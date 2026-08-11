0af9be0dcece44dbf9dba1d78c6ee791d5dd7e31
dvicoben <davidvicobenet2016@gmail.com>
2026-08-11 12:13:55 +0200

    adding license comments

--- src/slophep/FormFactors/FormFactorsBaryonic/FFOneHalfpToOneHalfpBase.py
+++ src/slophep/FormFactors/FormFactorsBaryonic/FFOneHalfpToOneHalfpBase.py
@@ -1,3 +1,26 @@
+# Copyright (C) 2026  David Vico Benet
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# SLOP or SLOPHEP employs, translates and/or reimplements utilities from:
+# - flavio (https://flav-io.github.io/), which is distributed under the MIT License, 
+# and without any warranty, see <https://mit-license.org/>
+# - Hammer (https://hammer.physics.lbl.gov/), which is distributed under version 3 of the GPL, 
+# and without any warranty, see <https://www.gnu.org/licenses/>
+# - EOS (https://eoshep.org/), which is distributed under version 2 of the GPL, 
+# and without any warranty, see <https://www.gnu.org/licenses/>
+
 from slophep.FormFactors.FormFactorBase import FormFactor
 from slophep.Tools.errfluct_tools import fluctsettings, FluctType
 
@@ -21,9 +44,9 @@ class FormFactorOneHalfpToOneHalfp(FormFactor):
         return self._C
     
     def w(self, q2: float) -> float:
-            mB = self.get_param(f"m_{self.B}")
-            mP = self.get_param(f"m_{self.P}")
-            return (mB**2 + mP**2 - q2) / (2 * mB * mP)
+        mB = self.get_param(f"m_{self.B}")
+        mP = self.get_param(f"m_{self.P}")
+        return (mB**2 + mP**2 - q2) / (2 * mB * mP)
     
     def get_ff(self, q2: float) -> dict:
         """Calculate form factors at particular q2.
--- src/slophep/__init__.py
+++ src/slophep/__init__.py
@@ -1,3 +1,26 @@
+# Copyright (C) 2026  David Vico Benet
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# SLOP or SLOPHEP employs, translates and/or reimplements utilities from:
+# - flavio (https://flav-io.github.io/), which is distributed under the MIT License, 
+# and without any warranty, see <https://mit-license.org/>
+# - Hammer (https://hammer.physics.lbl.gov/), which is distributed under version 3 of the GPL, 
+# and without any warranty, see <https://www.gnu.org/licenses/>
+# - EOS (https://eoshep.org/), which is distributed under version 2 of the GPL, 
+# and without any warranty, see <https://www.gnu.org/licenses/>
+
 import slophep.Core as Core
 import slophep.FormFactors as FormFactors
 import slophep.Observables as Observables
